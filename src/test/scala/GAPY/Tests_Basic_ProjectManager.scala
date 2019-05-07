902da6299a2ccb125705b22c72b13b3cfc9e2115
agegout <adrien.gegout@gmail.com>
2019-05-07 18:04:12 +0200

    Added one simple test to test the tests 8

--- src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
+++ src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
@@ -2,7 +2,7 @@ package GAPY
 
 import org.junit._
 import Assert._
-import GAPY._
+import scalaj.http._
 
 @Test
 class Test_Basic {
@@ -12,8 +12,10 @@ class Test_Basic {
         val gns3_server_url = "http://148.60.11.201"
         val gns3_server_port = "3080"
 
-        val all_projects = RESTCall("/v2/projects/" ,"GET")
-        assert(all_projects === "[]", "No project should exist")
+        val http = Http(gns3_server_url + gns3_server_port + "/v2/projects/")
+        val response: HttpResponse[String] = http.asString
+
+        assert(response === "[]", "No project should exist. Should had [] but had " + response)
     }
 
     @Test
