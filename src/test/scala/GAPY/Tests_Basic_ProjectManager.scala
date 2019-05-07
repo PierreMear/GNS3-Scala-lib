4a2065dfeaf047cca62443abcdc115eea2114571
agegout <adrien.gegout@gmail.com>
2019-05-07 17:15:27 +0200

    More advanced tests skeleton

--- src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
+++ src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
@@ -4,12 +4,25 @@ import org.junit._
 import Assert._
 
 @Test
-class Basic_Test {
+class Test_Basic {
 
     @Test
-    def testOK() = assertTrue(true)
+    def testOK() = {
+        /*
+        id_project = ""
+        val p = new ProjectManager(id_project,"http://148.60.11.201:3080")
+        p.addNode("PC1", "vpcs", "local")
+        .addNode("PC2", "vpcs", "local")
+        .addNode("PC3", "vpcs", "local")
+        .addNode("PC4", "vpcs", "local")
 
-//    @Test
-//    def testKO() = assertTrue(false)
+        var returned = RESTCall("/v2/projects/" + id_project + "/nodes","GET")
 
+
+        p.removeNode("PC1")
+        .removeNode("PC2")
+        .removeNode("PC3")
+        .removeNode("PC4")
+        */
+    }
 }
