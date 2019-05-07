2c0b5d0ebdaf0b7ced51ce2ffd7bd676bfe38633
agegout <adrien.gegout@gmail.com>
2019-05-07 18:14:42 +0200

    Added one simple test to test the tests 11

--- src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
+++ src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
@@ -12,7 +12,7 @@ class Test_Basic {
         val gns3_server_url = "http://148.60.11.201"
         val gns3_server_port = "3080"
 
-        val http = Http(gns3_server_url + gns3_server_port + "/v2/projects/")
+        val http = Http(gns3_server_url + ":" + gns3_server_port + "/v2/projects/")
         val response: HttpResponse[String] = http.asString
 
         assert(response.body == "[]", "No project should exist. Should had [] but had " + response)
