440f430d509b30064d3132b578c668ff09e20649
agegout <adrien.gegout@gmail.com>
2019-05-21 10:53:17 +0200

    Tentative de fix de JUNIT

--- src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
+++ src/test/scala/GAPY/Tests_Basic_ProjectManager.scala
@@ -18,24 +18,4 @@ class Test_Basic {
         assert(response.body == "[]", "No project should exist. Should had [] but had " + response)
     }
 
-    @Test
-    def testNodesBasic() = {
-        
-        /*
-        id_project = ""
-        val p = new ProjectManager(id_project,"http://148.60.11.201:3080")
-        p.addNode("PC1", "vpcs", "local")
-        .addNode("PC2", "vpcs", "local")
-        .addNode("PC3", "vpcs", "local")
-        .addNode("PC4", "vpcs", "local")
-
-        var returned = RESTCall("/v2/projects/" + id_project + "/nodes","GET")
-
-
-        p.removeNode("PC1")
-        .removeNode("PC2")
-        .removeNode("PC3")
-        .removeNode("PC4")
-        */
-    }
 }
