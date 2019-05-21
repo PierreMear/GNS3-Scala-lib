2bf17987ff68ef29f8775920ce2c830b806b860f
PierreMear <pierre.mear35@gmail.com>
2019-05-21 11:31:23 +0200

    Added the Bus Network topology template

---
+++ src/main/scala/topologies/BusNetwork.scala
@@ -0,0 +1,15 @@
+package topologies
+
+import GAPY.ProjectManager
+
+class BusNetwork(val nodeNamePrefix:String, val devices:List[String]) extends Topology{
+  override def create(projectManager:ProjectManager): Unit = {
+    projectManager.addNode(nodeNamePrefix+devices(0), nodetype, "local")
+    for(i <- 1 until devices.length){
+      val nodetype:String = devices(i)
+      projectManager.addNode(nodeNamePrefix+nodetype, nodetype, "local")
+      // TODO : The addLink function will be updated shorlty so this will have to be updated as well
+      projectManager.addLink(nodeNamePrefix+devices(i-1), 0, i, nodeNamePrefix+nodetype, 0, 0)
+    }
+  }
+}
