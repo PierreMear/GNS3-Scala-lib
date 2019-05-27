c2f67a6a43c183580004d631ced9538c2b11593c
PierreMear <pierre.mear35@gmail.com>
2019-05-27 14:47:15 +0200

    implementation de la fonction getProjectId qui renvoie l'id du projet à partir de son nom

--- src/main/scala/GAPY/GNS3_Manager.scala
+++ src/main/scala/GAPY/GNS3_Manager.scala
@@ -7,15 +7,15 @@ import GAPY.GNS3_Exceptions.InternalServerErrorException
 import GAPY.GNS3_Exceptions.UnknownException
 
 class GNS3_Manager(val serverAddress:String) {
-  
+
   def createProject(name : String) : ProjectManager = {
     val returned = RESTApi.post("/v2/projects", "{\"name\": \"" + name + "\"}",serverAddress);
-    val obj = JSONValue.parse(returned); 
+    val obj = JSONValue.parse(returned);
     val jsonObj:JSONObject = obj.asInstanceOf[JSONObject];
     val projectId = jsonObj.get("project_id").asInstanceOf[String];
     return new ProjectManager(projectId, serverAddress);
   }
-  
+
   def deleteProject(projectId: String) : GNS3_Manager = {
     val returned = RESTApi.delete("/v2/projects/" + projectId, serverAddress);
     val jsonObj:JSONObject = JSONValue.parse(returned).asInstanceOf[JSONObject];
@@ -29,9 +29,25 @@ class GNS3_Manager(val serverAddress:String) {
     }
     this
   }
-  
+
   def getProjectId(name : String) : String = {
-    return null
+    val returned = RESTApi.get("/v2/projects/", serverAddress);
+    val jsonObj:JSONObject = JSONValue.parse(returned).asInstanceOf[JSONObject];
+    val jsonProjet:JSONObject = obj.getJSONObject(name);
+    val statusObj = jsonObj.get("status");
+    if(jsonProjet != null){
+      return jsonProjet.getString("Id");
+    }else{
+      if(statusObj != null){
+        val status: String = statusObj.asInstanceOf[String];
+        status match {
+          case code if code.equals("404") => throw new NotFoundException("project not found");
+          case code if code.equals("500") => throw new InternalServerErrorException("server unreachable");
+        }
+      }
+      return null;
+    }
+
   }
-  
+
 }
