45d0c86ba4e4369a68e231758507675fe87dd53a
Yoann <yoann4997@hotmail.fr>
2019-05-27 15:01:50 +0200

    rework exception throw format

--- src/main/scala/GAPY/GNS3_Manager.scala
+++ src/main/scala/GAPY/GNS3_Manager.scala
@@ -5,6 +5,7 @@ import org.json.simple._
 import GAPY.GNS3_Exceptions.NotFoundException
 import GAPY.GNS3_Exceptions.InternalServerErrorException
 import GAPY.GNS3_Exceptions.UnknownException
+import GAPY.GNS3_Exceptions.ConflictException
 
 class GNS3_Manager(val serverAddress:String) {
 
@@ -13,6 +14,15 @@ class GNS3_Manager(val serverAddress:String) {
     val obj = JSONValue.parse(returned);
     val jsonObj:JSONObject = obj.asInstanceOf[JSONObject];
     val projectId = jsonObj.get("project_id").asInstanceOf[String];
+    val statusObj = jsonObj.get("status");
+    if(statusObj != null){
+      val status: Long = statusObj.asInstanceOf[Long];
+      status match {
+        case 404 => throw new NotFoundException("project not found");
+        case 500 => throw new InternalServerErrorException("server unreachable");
+        case 409 => throw new ConflictException("Conflict with existing project");
+       }
+    }
     return new ProjectManager(projectId, serverAddress);
   }
 
@@ -21,11 +31,12 @@ class GNS3_Manager(val serverAddress:String) {
     val jsonObj:JSONObject = JSONValue.parse(returned).asInstanceOf[JSONObject];
     val statusObj = jsonObj.get("status");
     if(statusObj != null){
-      val status: String = statusObj.asInstanceOf[String];
+      val status: Long = statusObj.asInstanceOf[Long];
       status match {
-        case code if code.equals("404") => throw new NotFoundException("project not found");
-        case code if code.equals("500") => throw new InternalServerErrorException("server unreachable");
-      }
+        case 404 => throw new NotFoundException("project not found");
+        case 500 => throw new InternalServerErrorException("server unreachable");
+        case 409 => throw new ConflictException("Conflict with existing project");
+       }
     }
     this
   }
@@ -33,16 +44,17 @@ class GNS3_Manager(val serverAddress:String) {
   def getProjectId(name : String) : String = {
     val returned = RESTApi.get("/v2/projects/", serverAddress);
     val jsonObj:JSONObject = JSONValue.parse(returned).asInstanceOf[JSONObject];
-    val jsonProjet:JSONObject = obj.getJSONObject(name);
+    val jsonProjet:JSONObject = jsonObj.getJSONObject(name);
     val statusObj = jsonObj.get("status");
     if(jsonProjet != null){
       return jsonProjet.getString("Id");
     }else{
       if(statusObj != null){
-        val status: String = statusObj.asInstanceOf[String];
+        val status: Long = statusObj.asInstanceOf[Long];
         status match {
-          case code if code.equals("404") => throw new NotFoundException("project not found");
-          case code if code.equals("500") => throw new InternalServerErrorException("server unreachable");
+          case 404 => throw new NotFoundException("project not found");
+          case 500 => throw new InternalServerErrorException("server unreachable");
+          case 409 => throw new ConflictException("Conflict with existing project");
         }
       }
       return null;
