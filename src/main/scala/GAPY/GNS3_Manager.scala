a27f80953727f13059c7aadbe371ecb4450f0ec0
Gwendal <gwendal.jouneaux@etudiant.univ-rennes1.fr>
2019-05-27 15:07:01 +0200

    change old ways of using JSON in managers

--- src/main/scala/GAPY/GNS3_Manager.scala
+++ src/main/scala/GAPY/GNS3_Manager.scala
@@ -10,16 +10,14 @@ class GNS3_Manager(val serverAddress:String) {
   
   def createProject(name : String) : ProjectManager = {
     val returned = RESTApi.post("/v2/projects", "{\"name\": \"" + name + "\"}",serverAddress);
-    val obj = JSONValue.parse(returned); 
-    val jsonObj:JSONObject = obj.asInstanceOf[JSONObject];
-    val projectId = jsonObj.get("project_id").asInstanceOf[String];
-    return new ProjectManager(projectId, serverAddress);
+    JSONApi.getFromObject(returned).getFromObject("project_id")
+    return new ProjectManager(JSONApi.value[String], serverAddress);
   }
   
   def deleteProject(projectId: String) : GNS3_Manager = {
     val returned = RESTApi.delete("/v2/projects/" + projectId, serverAddress);
-    val jsonObj:JSONObject = JSONValue.parse(returned).asInstanceOf[JSONObject];
-    val status: String = jsonObj.get("status").asInstanceOf[String];
+    JSONApi.getFromObject(returned).getFromObject("status")
+    val status: String = JSONApi.value[String]
     status match {
       case code if code.equals("404") => throw new NotFoundException("project not found");
       case code if code.equals("500") => throw new InternalServerErrorException("server unreachable");
