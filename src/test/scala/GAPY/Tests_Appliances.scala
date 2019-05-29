e28531151c903a34b5d634a13553d078f67b2f42
agegout <adrien.gegout@gmail.com>
2019-05-29 13:04:19 +0200

    Resolving import error

--- src/test/scala/GAPY/Tests_Appliances.scala
+++ src/test/scala/GAPY/Tests_Appliances.scala
@@ -3,6 +3,7 @@ package GAPY
 import org.junit._
 import Assert._
 import scalaj.http._
+import org.json.simple._ 
 
 
 //TODO dans tout lee fichier remplacer les nodes par des appliances avec les tests que vont bien, ce fichier n'est pour l'instant qu'un template
