5573b581c95d693f40676582cdc2bbd7cd2a223a
PierreMear <pierre.mear35@gmail.com>
2019-05-29 15:46:35 +0200

    fix : too many arguments in call for delete()

--- src/main/scala/GAPY/GNS3_Manager.scala
+++ src/main/scala/GAPY/GNS3_Manager.scala
@@ -58,7 +58,7 @@ class GNS3_Manager(val serverAddress:String, val username:String = "", val passw
    * @throws ConflictException if there is a conflict with an other project
    */
   def deleteProject(projectId: String) : GNS3_Manager = {
-    val returned = RESTApi.delete("/v2/projects/" + projectId, serverAddress,serverAddress,this.username,this.password)
+    val returned = RESTApi.delete("/v2/projects/" + projectId, serverAddress,this.username,this.password)
     if (returned != ""){
       JSONApi.parseJSONObject(returned).getFromObject("message");
       if(!JSONApi.isNullPointer()) {
@@ -121,7 +121,7 @@ class GNS3_Manager(val serverAddress:String, val username:String = "", val passw
    * @throws ConflictException if there is a conflict with an other project
    */
   def getProjectId(name : String) : String = {
-    val returned = RESTApi.get("/v2/projects", serverAddress,serverAddress,this.username,this.password)
+    val returned = RESTApi.get("/v2/projects", serverAddress,this.username,this.password)
     try {
       JSONApi.parseJSONArray(returned);
       val projects = JSONApi.value[JSONArray].toArray();
