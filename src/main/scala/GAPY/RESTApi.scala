96bb1ae024ff9c6cd23d102d15417ce4a972e624
PierreMear <pierre.mear35@gmail.com>
2019-05-29 16:50:39 +0200

    fix : types mismatch for http variable

--- src/main/scala/GAPY/RESTApi.scala
+++ src/main/scala/GAPY/RESTApi.scala
@@ -19,12 +19,8 @@ object RESTApi {
    * @return the body of the page requested
    */
   def get(url: String, serverAddress:String, user:String, pass:String): String = {
-    var http = null
-    if(user == "" && pass == ""){
-      http = Http(serverAddress + url)
-    }else{
-      http = Http(serverAddress + url).headers(("Authorization: Basic",user+ ":" + pass))
-    }
+    var http = Http(serverAddress + url)
+    if(user == "" && pass == "") http = Http(serverAddress + url).headers(("Authorization: Basic",user+ ":" + pass))
     val response: HttpResponse[String] = http.asString
     return response.body
   }
@@ -38,12 +34,8 @@ object RESTApi {
    * @return the body of the page requested
    */
   def post(url: String, body: String, serverAddress:String, user:String, pass:String): String = {
-    var http = null
-    if (user == "" && pass == ""){
-      http = Http(serverAddress + url)
-    }else{
-      http = Http(serverAddress + url).headers(("Authorization: Basic",user+ ":" + pass))
-    }
+    var http = Http(serverAddress + url)
+    if (user == "" && pass == "") http = Http(serverAddress + url).headers(("Authorization: Basic",user+ ":" + pass))
     val response: HttpResponse[String] = http.postData(body).asString
     return response.body
   }
@@ -56,12 +48,8 @@ object RESTApi {
    * @return the body of the page requested
    */
   def delete(url: String, serverAddress:String, user:String, pass:String): String = {
-    var http = null
-    if (user == "" && pass == ""){
-      http = Http(serverAddress + url)
-    }else{
-      http = Http(serverAddress + url).headers(("Authorization: Basic",user + ":" + pass))
-    }
+    var http = Http(serverAddress + url)
+    if (!(user == "" && pass == "")) http = Http(serverAddress + url).headers(("Authorization: Basic",user + ":" + pass))
     val response: HttpResponse[String] = http.method("delete").asString
     return response.body
   }
@@ -75,12 +63,8 @@ object RESTApi {
    * @return the body of the page requested
    */
   def put(url: String, body:String, serverAddress:String, user:String, pass:String): String = {
-    var http= null
-    if(user == "" && pass == ""){
-      http = Http(serverAddress + url)
-    }else{
-      http = Http(serverAddress + url).headers(("Authorization: Basic",user+ ":" + pass))
-    }
+    var http = Http(serverAddress + url)
+    if(user == "" && pass == "") http = Http(serverAddress + url).headers(("Authorization: Basic",user+ ":" + pass))
     val response: HttpResponse[String] = http.postData(body).method("put").asString
     return response.body
   }
