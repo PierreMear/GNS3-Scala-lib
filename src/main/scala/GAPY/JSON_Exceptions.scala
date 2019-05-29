b44f9856ed3ec11a6ef460ad9e1d66ca42825215
Gwendal <gwendal.jouneaux@etudiant.univ-rennes1.fr>
2019-05-29 05:48:09 +0200

    creation of the documentation for the JSONApi exception's

--- src/main/scala/GAPY/JSON_Exceptions.scala
+++ src/main/scala/GAPY/JSON_Exceptions.scala
@@ -2,8 +2,11 @@ package GAPY
 
 
 object JSON_Exceptions {
+  /** abstract class parent of all the errors linked to JSON */
   abstract class JSON_Exception(val message:String) extends Exception(message) {}
 
+  /** Exception throwed when you try to get from the wrong type of structure */
   case class JSONCastError(override val message:String) extends JSON_Exception(message)
+  /** Exception throwed when the last get resulted in a null */
   case class JSONNotExisting(override val message:String) extends JSON_Exception(message)
 }
