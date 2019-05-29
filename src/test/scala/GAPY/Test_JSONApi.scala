44600fbedcf999feeb39c17abf8f134c6aaefe8d
agegout <adrien.gegout@gmail.com>
2019-05-29 16:23:26 +0200

    Add some test for JSONApi

---
+++ src/test/scala/GAPY/Test_JSONApi.scala
@@ -0,0 +1,53 @@
+package GAPY
+
+import org.junit._
+import Assert._
+import scalaj.http._
+import org.json.simple._ 
+
+
+@Test
+class Test_JSONApi {
+
+    // Fonctions d'aides
+    def returnServerAddress(): String = {
+      return "http://148.60.11.201:3080"
+    }
+    
+    def returnUrlAPI(): String = {
+      return returnServerAddress() + "/v2/projects"
+    }
+    
+    def checkProjectsAPI(added:String): String = {
+      val http = Http(returnUrlAPI() + added)
+      val response: HttpResponse[String] = http.asString
+      
+      return response.body
+    }
+
+    @Test
+    def JSONError() = {
+
+        val returned = checkProjectsAPI("https://jsonplaceholder.typicode.com/todos/1")
+
+        var should = false
+        try {
+            val obj_fail = JSONApi.parseJSONArray(returned)
+        } catch {
+            case ev: JSONCastError => should = true
+            case ex: Exception => println("Erreur " + ex.printStackTrace()) ; should = true
+        }
+
+        assert(should, "Should had JSONCASTERROR")
+
+        should = false
+        try {
+            val obj_fail_2 = JSONApi.getFromObject(0)
+       } catch {
+            case ev: JSONNotExisting => should = true
+            case ex: Exception => println("Erreur " + ex.printStackTrace()) ; should = true
+        }
+
+        assert(should, "Should had JSONNotExistingError")
+    }
+}
