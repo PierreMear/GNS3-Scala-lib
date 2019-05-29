38ae29a9a9bc7f56fbebfd53c02a0e442fa28b1f
Gwendal <gwendal.jouneaux@etudiant.univ-rennes1.fr>
2019-05-29 05:38:17 +0200

    create the documentation of the JSONApi object

--- src/main/scala/GAPY/JSONApi.scala
+++ src/main/scala/GAPY/JSONApi.scala
@@ -4,20 +4,50 @@ import org.json.simple._
 import scala.util.control.Exception.Catch
 import JSON_Exceptions._
 
+/**
+ * @author Gwandalff
+ * 
+ * Small API to use with comfort the json simple API to read JSON file
+ */
 object JSONApi {
   
+  // The current object we're pointing on when we pass through the JSON
   private var pointer : Object = null
   
+  /**
+   * parseJSONObject : parse the JSON string in parameter and set pointer at the root of the JSON
+   * 
+   * Same as parseJSONArray but we made this to make it easyer to read the type for the next get
+   * 
+   * @param json_string the JSON as a String value
+   * @return {@link JSONApi} because we want this fluent
+   */
   def parseJSONObject(json_string:String) = {
     pointer = JSONValue.parse(json_string)
     this
   }
   
+  /**
+   * parseJSONArray : parse the JSON string in parameter and set pointer at the root of the JSON
+   * 
+   * Same as parseJSONObject but we made this to make it easyer to read the type for the next get
+   * 
+   * @param json_string the JSON as a String value
+   * @return {@link JSONApi} because we want this fluent
+   */
   def parseJSONArray(json_string:String) = {
     pointer = JSONValue.parse(json_string)
     this
   }
   
+  /**
+   * getFromObject : do a get on the pointer as if it was a {@link JSONObject}
+   * 
+   * @param paramName the name of the object's parameter we want to read
+   * @return {@link JSONApi} because we want this fluent
+   * @throws JSONNotExisting if the last get returned a null to us
+   * @throws JSONCastError if the pointer is not a {@link JSONObject}
+   */
   def getFromObject(paramName:String) = {
     if(pointer == null){
       throw JSONNotExisting("The JSON Object you wanted was null")
@@ -31,7 +61,15 @@ object JSONApi {
     this
   }
   
-  def getFromArray(paramName:Int) = {
+  /**
+   * getFromArray : do a get on the pointer as if it was a {@link JSONArray}
+   * 
+   * @param paramIndex the index of the arra's element we want to read
+   * @return {@link JSONApi} because we want this fluent
+   * @throws JSONNotExisting if the last get returned a null to us
+   * @throws JSONCastError if the pointer is not a {@link JSONArray}
+   */
+  def getFromArray(paramIndex:Int) = {
     if(pointer == null){
       throw JSONNotExisting("The JSON Array you wanted was null")
     }
@@ -44,10 +82,21 @@ object JSONApi {
     this
   }
   
+  /**
+   * value : get the value of the pointer at a specified type
+   * 
+   * @param T the type we want to cast then get the pointer
+   * @return the pointer as instance of T
+   */
   def value[T]() : T = {
     pointer.asInstanceOf[T]
   }
   
+  /**
+   * isNullPointer : check if the pointer is null
+   * 
+   * @return true if pointer is null else false
+   */
   def isNullPointer() : Boolean = {
     pointer == null
   }
